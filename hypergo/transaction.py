3d8e55067a1fbb3040412823c8f5494f7e744357
mattian7741 <github@mattian.com>
2023-09-08 08:59:54 -0400

    fixed parentid issue

--- hypergo/transaction.py
+++ hypergo/transaction.py
@@ -5,10 +5,10 @@ from hypergo.utility import Utility
 
 
 class Transaction:
-    def __init__(self, txid: Optional[str] = None, data: Optional[Dict[str, Any]] = None, parent: Optional["Transaction"] = None) -> None:
+    def __init__(self, txid: Optional[str] = None, data: Optional[Dict[str, Any]] = None, parentid: Optional[str] = None) -> None:
         self._txid: str = txid or Utility.unique_identifier()
         self._data: Dict[str, Any] = data or {}
-        self._parent: Optional[str] = parent
+        self._parentid: Optional[str] = parentid
 
     @staticmethod
     def from_str(txstr: str) -> "Transaction":
@@ -22,7 +22,7 @@ class Transaction:
         return str(self)
 
     def __str__(self) -> str:
-        return json.dumps(Utility.serialize({"txid": self._txid, "data": self._data, "parentid": self._parent.txid if self._parent else None}, None))
+        return json.dumps(Utility.serialize({"txid": self._txid, "data": self._data, "parentid": self._parentid}, None))
 
     def set(self, key: str, value: Any) -> None:
         Utility.deep_set(self._data, key, value)
@@ -31,10 +31,12 @@ class Transaction:
         return Utility.deep_get(self._data, key, default)
 
     def push(self):
-        return Transaction(parent=self)
+        return Transaction(parentid=self._txid)
 
 if __name__ == "__main__":
     tx = Transaction()
     print(str(tx))
     tx = tx.push()
     print(str(tx))
+    tx2 = Transaction.from_str('{"txid": "20230908125629539310017da948", "data": {}, "parentid": "2023090812562953908673e5d2ee"}')
+    print(str(tx2))
