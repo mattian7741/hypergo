05dd28e52e02124ffc1cfad8056f55986babed70
Ben Gray <ioncrash@gmail.com>
2024-04-17 16:38:37 -0400

    [DAT-39] add unhappy paths for TDD

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock
 
 from hypergo.validation import validation
 
-from hypergo.config import ConfigType
+from jsonschema import ValidationError
 
 
 class TestValidation(unittest.TestCase):
@@ -60,3 +60,104 @@ class TestValidation(unittest.TestCase):
                 }
              }
         )
+
+    def test_happy_path(self) -> None:
+        data = {
+             "message": {
+                  "body": {
+                       "some": "data"
+                  }
+             },
+             "config": {
+                "input_json_schema": {"$schema": "http://json-schema.org/draft-04/schema#",
+                    "type": "object",
+                    "properties": {"some": {"type": "string"}},
+                    "required": ["some"]
+                },
+                "output_json_schema": {"$schema": "http://json-schema.org/draft-04/schema#",
+                    "type": "object",
+                    "properties": {"some": {"type": "string"}},
+                    "required": ["some"]
+                }
+            }
+        }
+        result_generator = self._test_func(Mock(), data)
+        result_data = next(result_generator)
+
+        self.assertDictEqual(
+            result_data,
+            {
+                {
+                    "message": {
+                        "body": {
+                            "some": "modified data"
+                        }
+                    },
+                    "config": {
+                        "input_json_schema": {
+                            "$schema": "http://json-schema.org/draft-04/schema#",
+                            "type": "object",
+                            "properties": {"some": {"type": "string"}},
+                            "required": ["some"]},
+                        "output_json_schema": {"$schema": "http://json-schema.org/draft-04/schema#",
+                            "type": "object",
+                            "properties": {"some": {"type": "string"}},
+                            "required": ["some"]}
+                    }
+                }
+            }
+        )
+
+    def test_bad_input(self) -> None:
+        data = {
+             "message": {
+                  "body": {
+                       "some": -1
+                  }
+             },
+             "config": {
+                "input_json_schema": {"$schema": "http://json-schema.org/draft-04/schema#",
+                    "type": "object",
+                    "properties": {"some": {"type": "string"}},
+                    "required": ["some"]
+                },
+                "output_json_schema": {"$schema": "http://json-schema.org/draft-04/schema#",
+                    "type": "object",
+                    "properties": {"some": {"type": "string"}},
+                    "required": ["some"]
+                }
+            }
+        }
+
+        with self.assertRaises(ValidationError) as error:
+            result_generator = self._test_func(Mock(), data)
+            next(result_generator)
+
+        self.assertEquals(error.message, "Invalid input message")
+
+    def test_bad_output(self) -> None:
+        data = {
+             "message": {
+                  "body": {
+                       "some": "data"
+                  }
+             },
+             "config": {
+                "input_json_schema": {"$schema": "http://json-schema.org/draft-04/schema#",
+                    "type": "object",
+                    "properties": {"some": {"type": "string"}},
+                    "required": ["some"]
+                },
+                "output_json_schema": {"$schema": "http://json-schema.org/draft-04/schema#",
+                    "type": "object",
+                    "properties": {"missing": {"type": "string"}},
+                    "required": ["missing"]
+                }
+            }
+        }
+
+        with self.assertRaises(ValidationError) as error:
+            result_generator = self._test_func(Mock(), data)
+            next(result_generator)
+
+        self.assertEquals(error.message, "Invalid output message")
