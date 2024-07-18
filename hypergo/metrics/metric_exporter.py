c5b67ab69e796a88e0f4487b41d1eed0eaac60bb
VAnand <vipin.anand.cpp@gmail.com>
2024-07-18 00:04:52 -0400

    hypergo otel replacement

--- hypergo/metrics/metric_exporter.py
+++ hypergo/metrics/metric_exporter.py
@@ -7,6 +7,23 @@ class MetricExporter:
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         self.args = args
         self.kwargs = kwargs
+        self.__hash = self.__get_hash()
+
+    def __setattr__(self, __name: str, __value: Any) -> None:
+        if __name == "__hash":
+            raise AttributeError('''Can't set attribute "{0}"'''.format(__name))
+        elif __name == f"_{self.__class__.__name__}__hash":
+            if hasattr(self, __name):
+                raise AttributeError('''Can't set attribute "{0}"'''.format(__name))
+        object.__setattr__(self, __name, __value)
+
+    def __get_hash(self) -> int:
+        _hash: int = hash(None)
+        for arg in self.args:
+            _hash += hash(arg)
+        for _, v in sorted(self.kwargs.items(), key=lambda x: x[0]):
+            _hash += hash(v)
+        return _hash
 
     @abstractmethod
     def export(self, meter: str, metric_name: str, description: str,
@@ -17,9 +34,4 @@ class MetricExporter:
         return isinstance(__value, MetricExporter) and (self.kwargs == __value.kwargs) and (self.args == __value.args)
 
     def __hash__(self) -> int:
-        _hash: int = hash(None)
-        for arg in self.args:
-            _hash += hash(arg)
-        for _, v in sorted(self.kwargs.items(), key=lambda x: x[0]):
-            _hash += hash(v)
-        return _hash
+        return self.__hash
