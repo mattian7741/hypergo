3e5ca96be75018e18ff77e207621df2195c3259f
VAnand <vipin.anand.cpp@gmail.com>
2024-07-22 17:54:58 -0400

    hypergo otel replacement

--- hypergo/metrics/metric_exporter.py
+++ hypergo/metrics/metric_exporter.py
@@ -12,10 +12,16 @@ class MetricExporter:
             self.__setattr__(k, v)
         self.__hash = self.__get_hash()
         self.__result_set: List[Meter] = []
+        self.__is_shutdown: bool = False
 
     def __del__(self) -> None:
+        self.__is_shutdown = True
         if self.__result_set and len(self.__result_set):
             self.flush()
+
+    @property
+    def shutdown(self) -> bool:
+        return self.__is_shutdown
 
     def __setattr__(self, __name: str, __value: Any) -> None:
         if __name == "__hash":
