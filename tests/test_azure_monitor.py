ec7c16d8a9b68f9e4646116092d8de738ecba1bf
VAnand <vipin.anand.cpp@gmail.com>
2024-07-24 23:46:52 -0400

    hypergo otel replacement

--- tests/test_azure_monitor.py
+++ tests/test_azure_monitor.py
@@ -7,11 +7,14 @@ from unittest.mock import patch
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 
+
 from hypergo.message import MessageType
 from hypergo.config import ConfigType
 from hypergo.executor import Executor
 from hypergo.loggers.azure_logger import AzureLogger
 from hypergo.monitor import collect_metrics
+from hypergo.metrics.hypergo_metrics import HypergoMetrics
+from hypergo.metrics.otel_metrics_exporter import OtelMetricsExporter
 
 
 class TestAzureMonitor(unittest.TestCase):
@@ -30,10 +33,10 @@ class TestAzureMonitor(unittest.TestCase):
             pass
 
     @patch.dict(os.environ, {"APPLICATIONINSIGHTS-CONNECTION-STRING": f"InstrumentationKey={uuid4()};IngestionEndpoint=https://eastus2-3.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus2.livediagnostics.monitor.azure.com/"})
-    @patch("opentelemetry.sdk.metrics.MeterProvider.force_flush")
-    @patch("hypergo.metrics.hypergo_metrics.HypergoMetric.send")
+    @patch("hypergo.metrics.otel_metrics_exporter.OtelMetricsExporter.export")
+    @patch("hypergo.metrics.otel_metrics_exporter.OtelMetricsExporter.send")
     @patch("hypergo.secrets.LocalSecrets")
-    def test_azure_monitor(self, mock_secrets, mock_send, mock_collect):
+    def test_azure_monitor(self, mock_secrets, mock_send, mock_export):
         cfg: ConfigType = {
                                 "version": "2.0.0",
                                 "namespace": "datalink",
@@ -50,10 +53,11 @@ class TestAzureMonitor(unittest.TestCase):
         mock_secrets.get.return_value = os.environ["APPLICATIONINSIGHTS-CONNECTION-STRING"]
         # Create an instance of AzureLogger
         logger = AzureLogger(secrets=mock_secrets)
+        HypergoMetrics.set_metric_exporter(OtelMetricsExporter(metric_exporter=logger.metric_exporter))
         executor = Executor(cfg, logger=logger)
         self.__mock_send_message(executor=executor, message=self.message, config=cfg)
         assert mock_send.call_count == 5
-        mock_collect.assert_called_with(timeout_millis=60000)
+        assert mock_export.call_count == 5
 
 
 if __name__ == '__main__':
