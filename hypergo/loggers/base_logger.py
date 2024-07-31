449d7a858d7155dc6a59989b275e7d5e76f9a517
Ben Gray <ioncrash@gmail.com>
2024-07-31 15:21:12 -0400

    Add some extra info to the debugging logs

--- hypergo/loggers/base_logger.py
+++ hypergo/loggers/base_logger.py
@@ -51,7 +51,7 @@ class BaseLogger:
         self.log(message, level=logging.DEBUG, **kwargs)
 
     def info(self, message: str, **kwargs) -> None:
-        self.log("BEN IM LOGGING", level=logging.INFO)
+        self.log(f"BEN IM LOGGING in info, kwargs: {kwargs}", level=logging.INFO)
         self.log(message, level=logging.INFO, **kwargs)
 
     def warning(self, message: str, **kwargs) -> None:
